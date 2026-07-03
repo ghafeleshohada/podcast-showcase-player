b451a20b9f5933ce127e93a33789615bcb41fb23
amirghanbari <a.gh.gh69@gmail.com>
2026-07-03 22:29:11 +0330

    Add assets class

--- includes/assets.php
+++ includes/assets.php
@@ -1,1 +1,37 @@
 <?php
+/**
+ * Assets Loader
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class PSP_Assets {
+
+	public function __construct() {
+
+		add_action( 'wp_enqueue_scripts', array( $this, 'frontend_assets' ) );
+
+	}
+
+	public function frontend_assets() {
+
+		wp_register_style(
+			'psp-style',
+			PSP_URL . 'assets/css/player.css',
+			array(),
+			PSP_VERSION
+		);
+
+		wp_register_script(
+			'psp-player',
+			PSP_URL . 'assets/js/player.js',
+			array( 'jquery' ),
+			PSP_VERSION,
+			true
+		);
+
+	}
+
+}
