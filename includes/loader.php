122d32161212ac5b87d183d3b9835cef51bd0072
amirghanbari <a.gh.gh69@gmail.com>
2026-07-03 22:23:55 +0330

    Create Loader Class

--- includes/loader.php
+++ includes/loader.php
@@ -1,1 +1,59 @@
 <?php
+
+namespace PSP\Core;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class Loader
+ */
+class Loader {
+
+    /**
+     * Singleton instance.
+     *
+     * @var Loader|null
+     */
+    private static $instance = null;
+
+    /**
+     * Get instance.
+     *
+     * @return Loader
+     */
+    public static function instance() {
+
+        if ( self::$instance === null ) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+
+    }
+
+    /**
+     * Constructor.
+     */
+    private function __construct() {
+
+        add_action(
+            'init',
+            array( $this, 'init' )
+        );
+
+    }
+
+    /**
+     * Initialize plugin modules.
+     */
+    public function init() {
+
+        // Future:
+        // Register Elementor widgets.
+        // Register shortcodes.
+        // Register CPT.
+        // Register REST API.
+
+    }
+
+}
