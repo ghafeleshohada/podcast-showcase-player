d0def2ba097bd114f815688949b157fe70529821
amirghanbari <a.gh.gh69@gmail.com>
2026-07-03 22:30:31 +0330

    Add helper functions

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,1 +1,29 @@
 <?php
+/**
+ * Helper Functions
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Get plugin URL
+ */
+function psp_url( $path = '' ) {
+	return PSP_URL . ltrim( $path, '/' );
+}
+
+/**
+ * Get plugin path
+ */
+function psp_path( $path = '' ) {
+	return PSP_PATH . ltrim( $path, '/' );
+}
+
+/**
+ * Plugin version
+ */
+function psp_version() {
+	return PSP_VERSION;
+}
