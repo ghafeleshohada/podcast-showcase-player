17b0d0fd458677f3c38763c73ff44c3fbedb8b9c
amirghanbari <a.gh.gh69@gmail.com>
2026-07-03 22:21:46 +0330

    Create Plugin Core

--- includes/plugin.php
+++ includes/plugin.php
@@ -1,1 +1,81 @@
+<?php
+namespace PSP\Core;
 
+defined('ABSPATH') || exit;
+
+class Plugin {
+
+    /**
+     * Singleton instance.
+     *
+     * @var Plugin|null
+     */
+    private static $instance = null;
+
+    /**
+     * Get plugin instance.
+     *
+     * @return Plugin
+     */
+    public static function instance() {
+
+        if ( self::$instance === null ) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Prevent clone.
+     */
+    private function __clone() {}
+
+    /**
+     * Prevent unserialize.
+     */
+    public function __wakeup() {}
+
+    /**
+     * Constructor.
+     */
+    private function __construct() {}
+
+    /**
+     * Boot plugin.
+     */
+    public function boot() {
+
+        $this->load_files();
+
+        add_action(
+            'plugins_loaded',
+            array( $this, 'load_textdomain' )
+        );
+
+    }
+
+    /**
+     * Load plugin files.
+     */
+    private function load_files() {
+
+        Loader::instance();
+        Assets::instance();
+
+    }
+
+    /**
+     * Load translations.
+     */
+    public function load_textdomain() {
+
+        load_plugin_textdomain(
+            'podcast-showcase-player',
+            false,
+            dirname( plugin_basename( PSP_FILE ) ) . '/languages'
+        );
+
+    }
+
+}
